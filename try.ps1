0c344f8524f53f8c9688d532a1d543819aff1cb5
shivanipunj <shivanipunj04@gmail.com>
2023-11-15 22:57:35 +0530

    axas

--- try.ps1
+++ try.ps1
@@ -1,1 +1,10 @@
-echo "hellooooooooooooo world!"
+New-Item ProdServernames.csv -Force
+Set-Content ProdServernames.csv 'VmName,ResourceGroupName,SubscriptionName'
+$vmList = Get-AzVM
+foreach ($vm in $vmList) {
+$resourceGroupName = $vm.ResourceGroupName
+$ServerName = $vm.Name
+$subid = ($vm.Id -split '/')[2]
+$subscriptionname = (Get-AzSubscription -SubscriptionId $subid).Name
+Add-Content ProdServernames.csv "$ServerName,$resourceGroupName,$subscriptionname"
+}
